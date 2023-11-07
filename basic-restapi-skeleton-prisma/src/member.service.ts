9fe64cd15e1abb9921448810d90de38db84ea932
김재형 <cogito1016@naver.com>
2023-11-08 00:02:47 +0900

    feat: member service CRUD

--- basic-restapi-skeleton-prisma/src/member.service.ts
+++ basic-restapi-skeleton-prisma/src/member.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Member, Prisma } from '@prisma/client';
 import { PrismaService } from 'prisma.service';
 
 @Injectable()
 export class MemberService {
   constructor(private prisma: PrismaService) {}
+
+  async member(
+    memberWhereUniqueInput: Prisma.MemberWhereUniqueInput,
+  ): Promise<Member | null> {
+    return this.prisma.member.findUnique({
+      where: memberWhereUniqueInput,
+    });
+  }
+
+  async members(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.MemberWhereUniqueInput;
+    where?: Prisma.MemberWhereInput;
+    orderBy?: Prisma.MemberOrderByWithRelationInput;
+  }): Promise<Member[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.member.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createMember(data: Prisma.MemberCreateInput): Promise<Member> {
+    return this.prisma.member.create({
+      data,
+    });
+  }
+
+  async updateMember(params: {
+    where: Prisma.MemberWhereUniqueInput;
+    data: Prisma.MemberUpdateInput;
+  }): Promise<Member> {
+    const { where, data } = params;
+    return this.prisma.member.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteMember(
+    where: Prisma.MemberWhereUniqueInput,
+  ): Promise<Member | null> {
+    return this.prisma.member.delete({
+      where,
+    });
+  }
 }
