b6e42afab034fea4df654d1fbf5e2c6899781e16
김재형 <cogito1016@naver.com>
2023-11-14 18:28:59 +0900

    refactor: 필터 전역선언은 main에서만 해도 충분

--- login/login-api/src/app.module.ts
+++ login/login-api/src/app.module.ts
@@ -4,21 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { MemberService } from './member.service';
 import { AccountService } from './account.service';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    AppService,
-    PrismaService,
-    MemberService,
-    AccountService,
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
+  providers: [AppService, PrismaService, MemberService, AccountService],
 })
 export class AppModule {}
