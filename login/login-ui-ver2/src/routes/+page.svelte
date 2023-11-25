06c7bef0ee1441d8ec187d8d8909fe24166acb2e
김재형 <cogito1016@naver.com>
2023-11-25 20:00:18 +0900

    [login] feat: login-ui

--- login/login-ui-ver2/src/routes/+page.svelte
+++ login/login-ui-ver2/src/routes/+page.svelte
@@ -1,59 +1,97 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	let userId = "";
+	let password = "";
+  
+	
+	async function handleLogin() {
+		const loginEndpoint = 'http://localhost:3000/sign-in';
+		const user_id = userId;
+
+		const result = await fetch(loginEndpoint, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				user_id,
+				password,
+			}),
+		})
+
+		const data = await result.json();
+		console.log(result)
+		console.log(data)
+
+		if (result.ok) {
+			alert('로그인 성공');
+		} else {
+			alert(`로그인 실패 - ${data.msg}`);
+		}
+  }
+  
+	function handleSignUp() {
+								  window.location.href = '/sign-up';					
+	}
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+/* Add your styling here */
+.container {
+	max-width: 400px;
+	margin: auto;
+	padding: 20px;
+	border: 1px solid #ccc;
+	border-radius: 8px;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
-	h1 {
-		width: 100%;
-	}
+label {
+	display: block;
+	margin-bottom: 8px;
+}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+input {
+	width: 100%;
+	padding: 8px;
+	margin-bottom: 16px;
+	box-sizing: border-box;
+}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+button {
+	background-color: #4caf50;
+	color: white;
+	padding: 10px 15px;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+	margin-right: 8px;
+}
+
+button:hover {
+	background-color: #45a049;
+}
+
+.signup-button {
+	background-color: #008CBA;
+}
+
+.signup-button:hover {
+	background-color: #005A8C;
+}
 </style>
+  
+<div class="container">
+<h2>Login</h2>
+
+<form on:submit|preventDefault={handleLogin}>
+	<label for="userId">UserId:</label>
+	<input type="text" id="userId" bind:value={userId} />
+
+	<label for="password">Password:</label>
+	<input type="password" id="password" bind:value={password} />
+
+	<button type="submit">Login</button>
+	<button type="button" on:click={handleSignUp} class="signup-button">Sign Up</button>
+</form>
+</div>
+  
---
+++ login/login-ui-ver2/src/routes/sign-up/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+    let userId = "";
+    let password = "";
+    let name = "";
+    let mobilePhone = "";
+    let address = "";
+  
+    function handleRegistration() {
+      // Implement your registration logic here
+      const registrationData = {
+        user_id: userId,
+        password,
+        name,
+        mobile_phone: mobilePhone,
+        address,
+      };
+  
+      // Perform validation here if needed
+  
+      // Simulate registration success for demonstration purposes
+      alert('Registration successful!');
+      // You can redirect to a different page or perform other actions here
+    }
+  </script>
+  
+  <style>
+    /* Add your styling here */
+    .container {
+      max-width: 400px;
+      margin: auto;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+  
+    label {
+      display: block;
+      margin-bottom: 8px;
+    }
+  
+    input {
+      width: 100%;
+      padding: 8px;
+      margin-bottom: 16px;
+      box-sizing: border-box;
+    }
+  
+    button {
+      background-color: #4caf50;
+      color: white;
+      padding: 10px 15px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  
+    button:hover {
+      background-color: #45a049;
+    }
+  </style>
+  
+  <div class="container">
+    <h2>Membership Registration</h2>
+  
+    <form on:submit|preventDefault={handleRegistration}>
+      <label for="userId">User ID:</label>
+      <input type="text" id="userId" bind:value={userId} />
+  
+      <label for="password">Password:</label>
+      <input type="password" id="password" bind:value={password} />
+  
+      <label for="name">Name:</label>
+      <input type="text" id="name" bind:value={name} />
+  
+      <label for="mobilePhone">Mobile Phone:</label>
+      <input type="tel" id="mobilePhone" bind:value={mobilePhone} />
+  
+      <label for="address">Address:</label>
+      <textarea id="address" bind:value={address}></textarea>
+  
+      <button type="submit">Register</button>
+    </form>
+  </div>
+  
