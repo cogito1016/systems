2b321988c5db2aafeb9dc3377bd32c3baf0b5b5a
김재형 <cogito1016@naver.com>
2023-12-06 08:17:02 +0900

    [community] refactor: responseDTO & ReadonlyData활용한 결과값출력

--- community-cat-monorepo-mongodb/apps/cat-community/src/cat-community.controller.ts
+++ community-cat-monorepo-mongodb/apps/cat-community/src/cat-community.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { CatCommunityService } from './cat-community.service';
 import { JwtAuthGuard } from './jwt/jwt.guard';
 import { CurrentUser } from '@app/user/decorator/user.decorator';
+import { User } from '@app/user/user.schema';
+import { UserResponseDto } from '@app/user/dto/user.response.dto';
 
 @Controller('cat')
 export class CatCommunityController {
@@ -9,8 +11,8 @@ export class CatCommunityController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  getuser(@CurrentUser() user): string {
+  getuser(@CurrentUser() user: User): UserResponseDto {
     //현재접속한 사용자의 정보를 가져옴
-    return user;
+    return user.readOnlyData;
   }
 }
